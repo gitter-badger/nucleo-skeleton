8a0a6812193a7a01dce64d360053969c152862a1
robert <dobre_robert_marius@yahoo.com>
2015-05-14 18:25:41 +0200

    get version from package json

--- build/lib/actions/insert-license.js
+++ build/lib/actions/insert-license.js
@@ -4,13 +4,13 @@ var insertLicense = function(paths){
 
     var config = JSON.parse(fse.readFileSync(paths.config + 'build.config'));
     var mainFilePath = paths.root + config.mainLibPath + config.mainLibName;
-    var VERSION = fse.readFileSync(paths.root + 'VERSION', 'utf-8');
+    var package = JSON.parse(fse.readFileSync(paths.root + 'package.json', 'utf-8'));
     var mainFile = fse.readFileSync(mainFilePath, 'utf-8');
     var license = fse.readFileSync(paths.resources + 'license.txt', 'utf-8');
 
     license = license.replace('FRAMEWORK_NAME_PLACEHOLDER', config.name)
                         .replace('FRAMEWORK_URL_LICENSE', config.url_license)
-                        .replace('VERSION_STRING_PLACEHOLDER', VERSION);
+                        .replace('VERSION_STRING_PLACEHOLDER', package.version);
 
     mainFile = mainFile.replace('{{License}}', license);
 
