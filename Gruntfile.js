92a141b2e786e541e5f6fb0f263d83b4b573330d
robert <dobre_robert_marius@yahoo.com>
2015-06-06 09:07:55 +0200

    start implementing tdd for amd module

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    "babel": {
+    babel: {
       options: {
         modules: 'amdStrict',
         moduleIds: true
@@ -16,13 +16,23 @@ module.exports = function(grunt) {
           "ext": ".js"
        }]
       }
+    },
+    concat:{
+      mocha:{
+        src:['tests/unit/**/*.js'],
+        dest:'ephemeral/tests/demo/tests.js'
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task(s).
+  grunt.registerTask('build:tests:mocha', ['concat:mocha']);
+  grunt.registerTask('build:tests:jasmine', ['concat:mocha']);
+  grunt.registerTask('build:tests:qunit', ['concat:mocha']);
   grunt.registerTask('default', ['babel']);
 
 };
---
+++ tests/unit/no_group.js
@@ -0,0 +1,11 @@
+test('says ceva', function(){
+
+    check(define('ceva'), 'to.be', 'ceva');
+
+});
+
+test('says altceva', function(){
+
+    check(define('ceva'), 'to.be', 'ceva');
+
+});
