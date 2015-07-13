1eaf010b83b542c60f7c4fd8277ac630cd456f27
Dobre Robert <dobre_robert_marius@yahoo.com>
2015-07-13 12:07:52 +0200

    Update amd-wrapper.js

--- build/lib/resources/amd-wrapper.js
+++ build/lib/resources/amd-wrapper.js
@@ -2,3 +2,22 @@
 var define = AMD.define;
 var requirePackage = AMD.require;
 
+/*
+
+AMD define should be inside a wrapper with try and catch
+
+define = function(){
+  
+  try {
+    
+    AMD.define.apply(this, arguments);
+    
+  } catch(error){
+    
+    console.log(error);
+    
+  }
+  
+};
+
+*/
